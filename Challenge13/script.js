50414869359e7ab063a180dcaa4bf1bfa7d35cbd
rodrigo <contrerasagudo@gmail.com>
2023-03-23 22:00:45 -0300

    Challenge13

---
+++ Challenge13/script.js
@@ -0,0 +1,70 @@
+//Ejercicio N° 1
+let saludo1=("Hola ");
+let saludo2=("Soy un saludo");
+
+document.write(saludo1+saludo2);
+
+
+
+//Ejercicio N° 2
+//Asignacion de Numeros
+let num1=15;
+let num2=8;
+let num3=4;
+
+//Var aux
+par_o_no=undefined;
+
+
+//Suma
+let sum=(num1+num2);
+console.log(`La Suma es ${sum}`);
+
+par_o_no=sum%2==0 ? "es par":"es impar";
+console.log(par_o_no);
+
+let sum2=(sum+num3);
+console.log(`La Suma final es : ${sum2}`);
+
+
+//Resta
+let res=(num1-num2);
+console.log(`La Resta es ${res}`);
+
+par_o_no=res%2==0 ? "Es Par":"es Impar";
+console.log(par_o_no);
+
+let res2=(res-num3)
+console.log(`La Resta final es : ${res2}`);
+
+
+//Multiplicacion
+let mul=(num1*num2);
+console.log(`La Multiplicacion es ${mul}`);
+
+par_o_no=mul%2==0 ? "Es Par":"es Impar";
+console.log(par_o_no);
+
+let mul2=(mul*num3);
+console.log(`La Multiplicacion final es : ${mul2}`);
+
+
+//Division (esta no pide el uso del tercer numero)
+let div=(num1/num2);
+console.log(`La Division es ${div}`);
+
+par_o_no=div%2==0 ? "Es Par":"es Impar";
+console.log(par_o_no);
+
+
+
+
+
+
+
+
+
+
+
+
+
