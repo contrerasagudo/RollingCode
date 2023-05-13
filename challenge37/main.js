f653008bd64f20ff6713a9590d7b488a06d0abc4
Rodrigo Contreras Agudo <127220695+contrerasagudo@users.noreply.github.com>
2023-05-12 21:45:33 -0300

    Add files via upload

---
+++ challenge37/main.js
@@ -0,0 +1,46 @@
+
+function juego(){
+
+    boton=document.getElementById("boton1");
+    boton2=document.getElementById("boton2");
+    
+
+    var numeroAleatorio = Math.floor(Math.random()*100);
+
+   
+    var contador = 9;
+
+    var respuesta = (prompt('Introduzca un numero del 1 al 100'));
+
+    
+
+    while( respuesta!=numeroAleatorio && contador>0 ){
+        if(isNaN(respuesta) ||respuesta.trim()===""){
+            respuesta = prompt(`ingreso un caracter no valido , por querer bugear el codigo se le resta un turno igual , le quedan ${contador} intentos`);
+        }else if(respuesta>numeroAleatorio){
+            respuesta = prompt(`El Numero magico es menor , le quedan ${contador} intentos`);
+        }else{
+            respuesta = prompt(`El Numero magico es mayor , le quedan ${contador} intentos`);
+        }
+        contador--;
+    }
+
+    if(contador==0){
+        document.getElementById("resultado").innerHTML = 'Numero Magico = ' + numeroAleatorio;
+        alert("Agoto sus intentos");
+        boton.remove("disable")
+        boton2.disabled=false
+
+    }else{
+        document.getElementById("resultado").innerHTML = 'Numero Magico = ' + numeroAleatorio;
+        alert("Acerto el numero secreto");
+        boton.remove("disable")
+        boton2.disabled=false
+
+    }
+    
+}
+
+function rejugar(){
+    location.reload()
+}
