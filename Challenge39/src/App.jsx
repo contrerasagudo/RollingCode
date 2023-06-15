644feeac87273050b82831bedccc6c5d1b5132f2
contrerasagudo <contrerasagudo@gmail.com>
2023-06-14 21:36:36 -0300

    Challenge39

---
+++ Challenge39/src/App.jsx
@@ -0,0 +1,24 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Buttonsnav from './components/Buttonsnav'
+import Barrabusqueda from './components/Barrabusqueda'
+
+function App() {
+
+  return (
+    <>
+    <div className='Header'>
+      <Buttonsnav/>
+      <Barrabusqueda/>
+    </div>
+
+    
+      
+    </>
+  )
+}
+
+
+export default App
